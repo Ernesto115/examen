6ef64d80b1a8c4db243f0273b7676df13cade5e3
Ernesto Marquez De los Reyes <ernestodelosreyes51>
2025-03-07 17:01:34 -0600

    Examen de primera oportunidad

---
+++ multimedia.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=}, initial-scale=1.0">
+    <title>Multimedia</title>
+</head>
+<body>
+<h1>Aqui se insertan los archivos del examen</h1>
+<video src="/multimedia/Video 1.mp4" controls autoplays> </video>
+<audio src="/multimedia/Hero.m4a" controls autoplays ></audio>
+
+</body>
+</html>
